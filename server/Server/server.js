e278f627b9aa2ff4891e631235f25f93e406950d
fuzailxkhan <159718716+fuzailxkhan@users.noreply.github.com>
2024-02-12 11:53:19 +0500

    Add files via upload

---
+++ server/Server/server.js
@@ -0,0 +1,66 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const productSchema = require('./Schemas/productSchema');
+const orderSchema = require('./Schemas/orderSchema')
+
+
+const cors = require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/store")
+.then(()=>console.log("Connected To Database"))
+.catch((err)=>console.log("Database ran into",err.message))    
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDb Connection Error:'))
+db.once('open',()=>console.log("MongoDB connected Successfully"))
+
+
+
+const getAllProducts = async () =>{
+    const product  = await productSchema.find();
+    return product;
+}
+
+
+
+app.get("/products",async(req,res)=>{
+    console.log("req Recieved");
+    const allProducts = await getAllProducts();
+    const allProductsSend = [];
+    for (let i=0 ; i<allProducts.length ; i++){
+
+        const imgs = []
+        allProducts[i].images.forEach((img)=>{
+            imgs.push(fs.readFileSync(img))
+        })
+        
+        const product = {
+            id: allProducts[i].id,
+            title: allProducts[i].title,
+            price: allProducts[i].price,
+            description: allProducts[i].description,
+            image:imgs,
+        }
+        allProductsSend.push(product);
+    }
+    res.status(200);
+    res.json(allProductsSend);
+})
+
+app.post("/checkout",async (req,res)=>{
+    const shippingDetails = req.body.shipping;
+    const orderDetails = req.body.order;
+    const newOrder = orderSchema({name:shippingDetails.inputName,email:shippingDetails.inputEmail,phone:shippingDetails.inputNumber,address:shippingDetails.inputAddress,cod:shippingDetails.inputCOD,order:orderDetails});
+    await newOrder.save();
+    res.json("Confirmed")
+})
+
+app.listen(3000,(err)=>{
+    if(err)throw err;
+    console.log("Server Runing at Port 3000")
+})
